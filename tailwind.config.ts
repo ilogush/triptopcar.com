41b4bc29f4693188cfd45ccd78b6aaf4855d4c97
Artem <ilogush@icloud.com>
2024-12-13 13:02:16 +0200

    review accordion.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 import tailwindAnimate from "tailwindcss-animate";
+const flowbite = require("flowbite-react/tailwind");
 
 const config: Config = {
   darkMode: ["class"],
@@ -7,6 +8,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -94,6 +96,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [tailwindAnimate],
+  plugins: [
+      tailwindAnimate,
+      flowbite.plugin(),
+  ],
 };
 export default config;

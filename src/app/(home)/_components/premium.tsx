c37184258934dae0eefc5ce803989c1b528e8007
Logush <ilogush@icloud.com>
2024-12-25 11:41:32 +0700

    Update premium.tsx

--- src/app/(home)/_components/premium.tsx
+++ src/app/(home)/_components/premium.tsx
@@ -27,22 +27,22 @@ const PremiumConditions: React.FC<PremiumConditionsProps> = ({ car, className, i
           }}
       >
         <input type="checkbox" className="w-5 h-5 mr-2" readOnly checked={isSelected} />{" "}
-        Премиальный пакет
+        Premium Conditions
         <span className="text-base-black-secondary gap-2 font-normal text-sm ml-3">
             {formatPrice(premiumPrice)} в сутки
         </span>
       </h2>
       <ul className="list-disc text-slate-700 text-[15px] pl-5 font-medium mt-5">
-        <li>Беспоатная отмена бронирования за 2 дня</li>
-        <li>Полная страховка</li>
+        <li>Full insurance without excess</li>
+        <li>Theft and accident insurance</li>
         <li>Страхование от угона</li>
-        <li>Страхование гражданской ответственности</li>
-        <li>Страхование от несчастных случаев</li>
-        <li>Нет страховой франшизы</li>
-        <li>Неограниченное количество водителей</li>
-        <li>Пробег без ограниченийs</li>
-        <li>Круглосуточная онлайн-поддержка</li>
-        <li>Бесплатная помощь на дороге</li>
+        <li>Civil liability insurance</li>
+        <li>Unlimited number of drivers</li>
+        <li>Unlimited mileage</li>
+        <li>Departure from Phuket to a neighboring province</li>
+        <li>Support 24/7</li>
+        <li>Technical assistance on the road</li>
+        <li>Free cancellation of booking 3 days in advance</li>
       </ul>
     </div>
   );
